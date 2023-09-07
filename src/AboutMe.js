1bcfb4bd0e02a4d970fa82bba63bad99ab06d5ed
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2023-09-07 01:38:02 +0100

    typo

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -9,7 +9,7 @@ export default function AboutMe() {
             <br/>
             <p>As a Principal at King's Labs, I lead a team of software engineers to develop industry-level software solutions for innovative companies. I am also a research assistant at King's College London, where I work on projects related to machine learning and generative AI using Python, Java, and other programming languages. </p>
             <br/>
-            <p>In addition, I am a Computer Science Tutor, offering tailored one-to-one sessions for students up to A Level or equivalent, covering topics such as programming, data structures, amdorithms, and web development. I have a passion for teaching and mentoring, and I have helped many students achieve their academic goals and improve their coding skills. </p>
+            <p>In addition, I am a Computer Science Tutor, offering tailored one-to-one sessions for students up to A Level or equivalent, covering topics such as programming, data structures, algorithms, and web development. I have a passion for teaching and mentoring, and I have helped many students achieve their academic goals and improve their coding skills. </p>
             <br/>
             <p>I am currently pursuing a BSc in Computer Science with Management and a Year in Industry at King's College London. I have a stellar academic record, having won several awards and scholarships, such as the Computer Science with Management Prize 22-23, the Merlyn Lowther Scholarship Award, and the Christine Lee Jones Prize for Leadership and Initiative. </p>
             <br/>
