cb6e3c296d26ca51f35d475ea1c26e2825a8c448
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-08-05 00:54:08 +0530

    pull layout stuff out of Window component

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function Window({children}) {
+export default function Window({children, className}) {
     return (
-        <div className="w-full md:container mx-auto rounded-lg bg-pink-200 border-2 border-gray-500 p-2 drop-shadow-lg flex flex-col gap-3">
+        <div className={"rounded-lg bg-pink-200 border-2 border-gray-500 p-2 drop-shadow-lg flex flex-col gap-3 " + className }>
             <div className={"flex gap-2 pt-1"}>
                 <div className={"bg-red-500 border-2 border-red-600 rounded-full h-4 w-4"}></div>
                 <div className={"bg-amber-400 border-2 border-amber-500 rounded-full h-4 w-4"}></div>
                 <div className={"bg-green-400 border-2 border-green-500 rounded-full h-4 w-4"}></div>
             </div>
-            <div className={"flex flex-col md:grid md:grid-rows-1 md:grid-cols-4 h-full bg-white rounded-lg p-4 border-2 border-gray-400"}>
+            <div className={"h-full w-full bg-white rounded-lg p-4 border-2 border-gray-400"}>
                 {children}
             </div>
         </div>
